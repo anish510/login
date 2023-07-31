f420154d6737e6928fd17b9a8381d683fe773210
Anish <anishjoshi510@gmail.com>
2023-07-31 23:14:19 +0545

    changes

--- log/urls.py
+++ log/urls.py
@@ -3,9 +3,9 @@ from. import views
 
 urlpatterns = [
     path('',views.index,name = 'index'),
-    path('home',views.home, name = 'home'),
-    path('register',views.register,name= 'register'),
-    path('login',views.login,name = 'login'),
-    path('logout',views.logout,name ='logout'),
+    path('home/',views.home, name = 'home'),
+    path('register/',views.register,name= 'register'),
+    path('login/',views.login,name = 'login'),
+    path('logout/',views.logout,name ='logout'),
     
 ]
